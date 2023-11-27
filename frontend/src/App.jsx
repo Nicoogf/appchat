67e5440cd8a3de92db56ea28e661e07a7081279b
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-27 19:52:06 -0300

    primer commit del proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+   
+      <div className='text-red-500'>
+        <h1 className='text-red-500'> Hola </h1>
+      </div>
+      
+  )
+}
+
+export default App
